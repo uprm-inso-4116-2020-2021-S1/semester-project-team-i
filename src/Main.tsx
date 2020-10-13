740b2eb7515588c61b04f45335670544e73ce8a0
David <david@iMac-de-Tito.local>
2020-10-13 12:03:52 -0400

    empece pagina de cada plato

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,12 +3,14 @@ import { LandingPage } from './LandingPage';
 import './Main.css';
 import { initializeIcons } from '@uifabric/icons';
 import { ExplorePage } from './ExplorePage';
+import { Item } from './Item';
 
 initializeIcons();
 
 interface MainState {
     isLandingPageVisible: boolean;
     isExploreVisible: boolean;
+    isItemVisible: boolean;
 }
 
 export class Main extends React.Component<{}, MainState> {
@@ -17,7 +19,8 @@ export class Main extends React.Component<{}, MainState> {
         super(props);
         this.state ={
             isLandingPageVisible: true,
-            isExploreVisible: false
+            isExploreVisible: false,
+            isItemVisible: false
         }
     }
 
@@ -25,20 +28,33 @@ export class Main extends React.Component<{}, MainState> {
         return (
             <body>
                 <div className="navbar">
+                   
                     <span className="title" onClick={()=>
                         {this.setState({
                             isExploreVisible : false,
-                            isLandingPageVisible: true
+                            isLandingPageVisible: true,
+                            isItemVisible: false
                         })}}>Find &amp; Eat</span>
+
                     <div className="explore" onClick={()=>
                         {this.setState({
                             isExploreVisible : true,
-                            isLandingPageVisible: false
+                            isLandingPageVisible: false,
+                            isItemVisible: false
                         })}}></div>
-                    <div className="profile"></div>
+
+                    <div className="profile" onClick={()=>
+                    {this.setState({
+                        isExploreVisible : false,
+                        isLandingPageVisible: false,
+                        isItemVisible: true
+                    })}}>
+                    </div>
+
                 </div>
                 {this.state.isLandingPageVisible && <LandingPage></LandingPage>}
                 {this.state.isExploreVisible && <ExplorePage></ExplorePage>}
+                {this.state.isItemVisible && <Item></Item>}
             </body>
             
         )
