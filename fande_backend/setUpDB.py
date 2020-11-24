64fbda3e9383862d993066597622536460d77fd4
Everod52 <everson.rodriguez@upr.edu>
2020-11-23 22:21:41 -0400

    Merged changes from gabriel's branch. Added timestamp default in review and upvote attributes.

--- fande_backend/setUpDB.py
+++ fande_backend/setUpDB.py
@@ -5,8 +5,8 @@ from modules.dish.dish_dao import Dish
 from modules.category.category_dao import Category, create_categories
 from modules.establishment.establishment_dao import Establishment
 from modules.user.user_dao import User
-# from modules.review.review_dao import Review
-# from modules.upvote.upvote_dao import UpVote
+from modules.review.review_dao import Review
+from modules.upvote.upvote_dao import UpVote
 
 db.drop_all()
 db.create_all()
